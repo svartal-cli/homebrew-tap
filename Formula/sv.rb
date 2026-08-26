18e81b69adb711a7939151b0f6e84dfb17a01d71
Ivaldi Desktop <28c91313-b516-4770-85cd-1be21f389138@sessions.t3.local>
2026-08-26 05:11:26 -0500

    sv 0.1.2

--- Formula/sv.rb
+++ Formula/sv.rb
@@ -1,26 +1,26 @@
 class Sv < Formula
   desc "Terminal front end for Svartal: sign in, list machines, open shells"
   homepage "https://github.com/svartal-cli/svartal-cli"
-  version "0.1.1"
+  version "0.1.2"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/svartal-cli/svartal-cli/releases/download/v#{version}/sv-v#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "1f82d2d6ecf1d0f0a68635b375a500088557efae8b78d092f818d8e84e9c5d78"
+      sha256 "9caf0a5e7b2fb39026288df315e7e0cd3e765b4d6dcbed8f4a25879cde9dcce9"
     else
       url "https://github.com/svartal-cli/svartal-cli/releases/download/v#{version}/sv-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "cc05f9a6281fc1b8af38764d84e66b798ee254eb9db05c1c07d0f22582398188"
+      sha256 "62048f37a6bb8f512c594c2a29bd6d37ea6de6ac4d3ab49ed915183a289a8484"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/svartal-cli/svartal-cli/releases/download/v#{version}/sv-v#{version}-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "01542c1f1fa7ce30880cf133e81a894acb577df239928b6009ac4122ef38918e"
+      sha256 "577104aaeaad36f12822dba908ca77eb71f8350b603538870ebe980e794c8ed7"
     else
       url "https://github.com/svartal-cli/svartal-cli/releases/download/v#{version}/sv-v#{version}-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "9f794884e9dd962be52ef93ab7945b990a9564cbf7ab3022de622351d2eb27e3"
+      sha256 "1aa87a4418ca89d29c619d8a405a4e75d442507ff1e090e3cad0516294865dae"
     end
   end
 
